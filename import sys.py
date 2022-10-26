cfb2d03898f26b1fb9294fe66ca92c7256494784
houalla88 <oualla.houbayda@gmail.com>
2022-10-26 12:03:03 +0200

    correction de données

--- import sys.py
+++ import sys.py
@@ -4,8 +4,8 @@ print(sys.version)
 print(sys.executable)
 
 
-#def greet(who_to_greet):
-#    greeting = 'Hello,{}'.format(who_to_greet)
-#    return greeting
+def greet(who_to_greet):
+    greeting = 'Hello,{}'.format(who_to_greet)
+    return greeting
 
 print(greet('Houba'))
